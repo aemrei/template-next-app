659bf53bf1d2f0babd894b57ba75e4d08b171691
Emre INEL <aemreinel@gmail.com>
2022-06-29 07:45:44 +0300

    Add tsconfig paths to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,35 @@
+const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
-  stories: ["../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../containers/**/*.stories.mdx",
+    "../containers/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    [].push.apply(config.resolve.plugins, [
+      new TsconfigPathsPlugin({ extensions: config.resolve.extensions }),
+    ]);
+
+    return config;
   },
 };
